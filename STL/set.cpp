4a0455f6dbb8b5ee98f4368ffa3d2cd45b33bf0c
Tiago Silva <tiago.pds.silva@gmail.com>
2023-06-15 10:20:24 -0300

    Create set.cpp

---
+++ STL/set.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    // funciona como um conjunto, não possui elementos repetidos, (quase) todas as funções são O(logN)
+    set<int> s;
+
+    s.insert(1); //insere elemento
+
+    s.erase(1); //remove elemento(se não houver esse elemento, vai ocorrer um erro)
+
+    s.clear(); //remove todos os elementos; 0(N)
+
+    auto it = s.find(1); //ponteiro para um elemento com mesmo valor de x, se não houver aponta para s.end()
+
+    auto it = s.lower_bound(1); //ponteiro para o primeiro elemento com valor maior ou igual a x, se não houver aponta para s.end();
+
+    auto it = s.upper_bound(1); //ponteiro para o primeiro elemento com valor maior que x, se não houver aponta para s.end();
+
+    s.erase(it); //também pode usar erase em um iterador
+
+    return 0;
+}
