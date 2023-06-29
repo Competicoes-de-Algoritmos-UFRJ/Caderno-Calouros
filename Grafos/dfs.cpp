ecbd8d4c886294054859abfa6d1a1f78039a0b82
Tiago Silva <tiago.pds.silva@gmail.com>
2023-06-29 11:28:40 -0300

    Update dfs.cpp

--- Grafos/dfs.cpp
+++ Grafos/dfs.cpp
@@ -1,7 +1,14 @@
+const int mxn = (int) 1e5+5;
+
+// vis[i] indica se o vértice i já foi visitado, se for igual a 1, quer dizer que sim, se for igual a 0, não
+// adj[i] é a lista de adjacência do vértice i
+int vis[mxn];
+vector<int> adj[mxn];
+
 void dfs(int x){
     vis[x]=1;
 
-    for(int i:vizinhos[x]){
+    for(int i:adj[x]){
         if(vis[i]==0){
             dfs(i);
         }
