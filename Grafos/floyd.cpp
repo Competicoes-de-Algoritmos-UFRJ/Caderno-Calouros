d945b381c31999cf9407cd4269de97949203bf7e
Tiago Silva <tiago.pds.silva@gmail.com>
2023-08-21 22:23:48 -0300

    Update floyd.cpp

--- Grafos/floyd.cpp
+++ Grafos/floyd.cpp
@@ -1,8 +1,8 @@
- while(m--){
-	 ll a,b,c; cin >> a >> b >> c; a--; b--;
-	 graph[a][b] = min(graph[a][b], c);
-	 graph[b][a] = min(graph[b][a], c);
- }
+while(m--){
+	ll a,b,c; cin >> a >> b >> c; a--; b--;
+	graph[a][b] = min(graph[a][b], c);
+	graph[b][a] = min(graph[b][a], c);
+}
 
 for(int i = 0; i < n; i++) graph[i][i] = 0;
 
